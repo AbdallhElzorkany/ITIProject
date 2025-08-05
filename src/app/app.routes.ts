1fea1a9b605e741450e43f453ea7384114a3c45b
Abdallh Elzorkany <abdoh5479@gmail.com>
2025-08-05 18:32:03 +0300

    routing configuration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from '../components/home-component/home-component';
+import { AboutComponent } from '../components/about-component/about-component';
+import { LatestPostsComponent } from '../components/latest-posts-component/latest-posts-component';
+import { NotFoundComponent } from '../components/not-found-component/not-found-component';
+import { PostComponent } from '../components/post-component/post-component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path:"",
+    component:HomeComponent
+  },
+  {
+    path:"about",
+    component:AboutComponent
+  },
+  {
+    path:"latest-posts",
+    component:LatestPostsComponent
+  },
+  {
+    path:"post/:name",
+    component:PostComponent
+  },
+  {
+    path:"**",
+    component:NotFoundComponent,
+    pathMatch:"full"
+  }
+];
---
+++ src/components/latest-posts-component/latest-posts-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-latest-posts-component',
+  imports: [],
+  templateUrl: './latest-posts-component.html',
+  styleUrl: './latest-posts-component.css'
+})
+export class LatestPostsComponent {
+
+}
