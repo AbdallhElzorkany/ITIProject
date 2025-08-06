f4ca9f69c5ccfb213843efec5ece3010210e91ff
Abdallh Elzorkany <abdoh5479@gmail.com>
2025-08-07 02:55:39 +0300

    latestPosts home

--- src/components/home-component/home-component.ts
+++ src/components/home-component/home-component.ts
@@ -18,41 +18,62 @@ export class HomeComponent {
       img: 'helmut.jpeg',
       category: 'PHOTOGRAPHY',
       title: 'HELMUT LANGCELEBRATES TAXIDRIVERS WORLDWIDE INLATEST CAMPAIGN',
-      author: 'BY ALESSANDRA ORTIZ',
+      author: 'ALESSANDRA ORTIZ',
     },
     {
       img: 'bowlcut.jpeg',
       category: 'PHOTOGRAPHY',
       title:
         'BOWLCUT LAUNCH A NEW SUMMER COLLECTION THAT PAYS HOMAGE TO "UK LEGENDS"',
-      author: 'BY ROSANNA ONDRICKA',
+      author: 'ROSANNA ONDRICKA',
     },
     {
       img: 'thousands.jpeg',
       category: 'PHOTOGRAPHY',
       title:
         'THOUSANDS OF PREVIOUSLY UNSEEN PHOTOGRAPHS BY ANDY WARHOL WILL BE MADE PUBLIC THIS AUTUMN',
-      author: 'BY ANNIE LUEILWITZ',
+      author: 'ANNIE LUEILWITZ',
     },
     {
       img: 'london.jpeg',
       category: 'INTERACTIVE DESIGN',
       title: "LONDON-BASED YINKA ILORI'S STORYTELLING FURNITURE",
-      author: 'BY ANNIE LUEILWITZ',
+      author: 'ANNIE LUEILWITZ',
     },
     {
       img: 'anonymous.jpeg',
       category: 'GRAPHIC DESIGN',
       title:
         'ANONYMOUS ISRAELI ART COLLECTIVE BROKEN FINGAZ DIRECT MUSIC VIDEO FOR U2 AND BECK',
-      author: 'BY SIMEON BREKKE',
+      author: 'SIMEON BREKKE',
     },
     {
       img: 'suzzanne.jpeg',
       category: 'ARCHITECTURE',
       title:
         "SUZANNE SAROFF'S METICULOUSLY ARRANGED PHOTOGRAPHS ALTER PERCEPTIONS",
-      author: 'BY RETA TORPHY',
+      author: 'RETA TORPHY',
+    },
+  ];
+  public latestPosts: {
+    img: string;
+    category: string;
+    title: string;
+    author: string;
+  }[] = [
+    {
+      img: 'japan.jpeg',
+      category: 'Illustration',
+      title: 'Japan House opens in mountainside to foster peak creativity.',
+      author: 'RETA TORPHY',
+    },
+    ...this.firstSection,
+    {
+      img: 'anu.jpeg',
+      category: 'GRAPHIC DESIGN',
+      title:
+        'Anu Ambasna’s playful illustrations celebrate club culture, brown bodies and perfect paunches',
+      author: 'Leo Bartell',
     },
   ];
   constructor() {}
