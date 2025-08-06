8fd5d865bd65f09b60041c04deb9b696dba228be
Abdallh Elzorkany <abdoh5479@gmail.com>
2025-08-06 04:16:37 +0300

    header component

--- src/components/header-component/header-component.ts
+++ src/components/header-component/header-component.ts
@@ -1,11 +1,19 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header-component',
-  imports: [],
+  imports: [RouterLink, NgClass],
   templateUrl: './header-component.html',
-  styleUrl: './header-component.css'
+  styleUrl: './header-component.css',
 })
 export class HeaderComponent {
-
+  public collapse: boolean;
+  constructor() {
+    this.collapse = false;
+  }
+  toggleCollapse() {
+    this.collapse = !this.collapse;
+  }
 }
