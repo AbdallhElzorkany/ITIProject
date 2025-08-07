e5b395372ebd7225681d19b387f829669c0391ce
Abdallh Elzorkany <abdoh5479@gmail.com>
2025-08-07 07:32:37 +0300

    about-notfound done

--- src/components/about-component/about-component.ts
+++ src/components/about-component/about-component.ts
@@ -4,8 +4,94 @@ import { Component } from '@angular/core';
   selector: 'app-about-component',
   imports: [],
   templateUrl: './about-component.html',
-  styleUrl: './about-component.css'
+  styleUrl: './about-component.css',
 })
 export class AboutComponent {
+  public team: {
+    name: string;
+    img: string;
+    title: string;
+  }[];
+  public touch: {
+    num: number;
+    title: string;
+    description: string;
+  }[];
+  constructor() {
+    this.team = [
+      {
+        name: 'Simeon Peterson',
+        img: 'simeon.jpg',
+        title: 'Cofounder, Editor in Chief',
+      },
+      {
+        name: 'Simeon Brekke',
+        img: 'brekke.jpg',
+        title: 'Cofounder, Partner',
+      },
+      {
+        name: 'Annie Lueilwitz',
+        img: 'annie.jpg',
+        title: 'Managing Editor',
+      },
+      {
+        name: 'reta torphy',
+        img: 'reta.jpg',
+        title: 'Managing Editor',
+      },
+      {
+        name: 'Leo Bartell',
+        img: 'leo.jpg',
+        title: 'Editor',
+      },
+      {
+        name: 'Clem Onojeghuo',
+        img: 'clem.jpg',
+        title: 'Senior Writer',
+      },
+      {
+        name: 'Michal Legros',
+        img: 'legros.jpg',
+        title: 'Senior Writer',
+      },
+      {
+        name: 'Rosanna Ondricka',
+        img: 'rosanna.jpg',
+        title: 'Writer',
+      },
 
+      {
+        name: 'Alessandra Ortiz',
+        img: 'ortiz.jpg',
+        title: 'Writer',
+      },
+      {
+        name: 'Coby Gottlieb',
+        img: 'coby.jpg',
+        title: 'Writer',
+      },
+    ];
+    this.touch = [
+      {
+        num: 1,
+        title: 'address',
+        description: '144 N 7th St. #536 Brooklyn, NY 11249 United States',
+      },
+      {
+        num: 2,
+        title: 'ageneral INQUIRIES',
+        description: 'hello@noiceland.co',
+      },
+      {
+        num: 3,
+        title: 'PRESS inquiries',
+        description: 'press@noiceland.co',
+      },
+      {
+        num: 4,
+        title: 'potential contributors',
+        description: 'collab@noiceland.co',
+      },
+    ];
+  }
 }
