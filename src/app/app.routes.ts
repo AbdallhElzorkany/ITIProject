7acff80491ba4b6cbeb75f1b1c2588d337ff4998
Abdallh Elzorkany <abdoh5479@gmail.com>
2025-08-08 01:30:42 +0300

    PROJECT DONE

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,28 +3,23 @@ import { HomeComponent } from '../components/home-component/home-component';
 import { AboutComponent } from '../components/about-component/about-component';
 import { LatestPostsComponent } from '../components/latest-posts-component/latest-posts-component';
 import { NotFoundComponent } from '../components/not-found-component/not-found-component';
-import { PostComponent } from '../components/post-component/post-component';
 
 export const routes: Routes = [
   {
-    path:"",
-    component:HomeComponent
+    path: '',
+    component: HomeComponent,
   },
   {
-    path:"about",
-    component:AboutComponent
+    path: 'about',
+    component: AboutComponent,
   },
   {
-    path:"latest-posts",
-    component:LatestPostsComponent
+    path: 'latest-posts',
+    component: LatestPostsComponent,
   },
   {
-    path:"post/:name",
-    component:PostComponent
+    path: '**',
+    component: NotFoundComponent,
+    pathMatch: 'full',
   },
-  {
-    path:"**",
-    component:NotFoundComponent,
-    pathMatch:"full"
-  }
 ];
